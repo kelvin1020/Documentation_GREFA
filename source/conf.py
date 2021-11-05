c2707be0daa5965ad96ab13822efe3c387aa9b01
Shucheng Yang <kelvin1020@foxmail.com>
2021-11-05 20:04:28 +0800

    reset, may fix bugs

--- source/conf.py
+++ source/conf.py
@@ -31,6 +31,7 @@ release = '0.90'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'recommonmark',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -41,12 +42,12 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'Zh_CN'
+language = 'zh_CN'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -54,12 +55,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-source_suffix = ['.rst', '.md', '.MD']
-html_theme = 'sphinx_rtd_theme'#'alabaster'
-# source_parsers = {
-#     '.md': CommonMarkParser,
-#     '.MD': CommonMarkParser,
-# }
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
